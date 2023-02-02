6a0fc5c6f47559fd0aedca8324936c4a7c1b4154
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-02-01 22:15:45 -0700

    Add: update index_spec + show_spec for parent, user story 1 + 2 tests created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'home#index'
+  # get '/', to: 'home#index'
   get '/magic_decks', to: 'magic_decks#index'
+  get '/magic_decks/:id', to: 'magic_decks#show'
   get '/cards', to: 'cards#index'
 
 end
